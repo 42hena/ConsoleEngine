29d69739c66f4c0cbd2766e82ae1c24a4fbc97b8
soloplay <somnium1108@gmail.com>
2025-07-24 09:19:18 +0900

    레벨 클래스 구현
    - Actor들을 관리하는 Level 클래스 구현

---
+++ Engine/Level/Level.cpp
@@ -0,0 +1,54 @@
+#include "Level.h"
+#include "Actor/Actor.h"
+
+/*
+*		특수 멤버 함수
+*/
+
+Level::Level()
+{
+}
+
+Level::~Level()
+{
+	for (Actor* const actor : _actors)	{
+		delete actor;
+	}
+}
+
+/*
+*		이벤트 함수
+*/
+
+void Level::BeginPlay()
+{
+	for (Actor* const actor : _actors)
+	{
+		if (actor->HasBeginPlay() == true) { continue; }
+
+		actor->BeginPlay();
+	}
+}
+
+void Level::Tick(float deltaTime)
+{
+	for (Actor* const actor : _actors) {
+		actor->Tick(deltaTime);
+	}
+}
+
+void Level::Render()
+{
+	for (Actor* const actor : _actors) {
+		actor->Render();
+	}
+}
+
+/*
+*		객체 관리 함수
+*/
+
+void Level::AddActor(Actor* newActor)
+{
+	_actors.push_back(newActor);
+}
