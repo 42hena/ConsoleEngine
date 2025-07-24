f85cd51cb3a10e31ee923ab4141d60d3daf97341
soloplay <somnium1108@gmail.com>
2025-07-24 09:27:58 +0900

    엔진 기능 추가
    - 키 입력 관련 구조체 추가
    - 키 입력 이벤트 추가
    - Level 시스템 도입
    - 함수 이름 변경 Update -> Tick
    - 객체 시작 이벤트 추가

--- Engine/Core/Engine.cpp
+++ Engine/Core/Engine.cpp
@@ -1,8 +1,28 @@
 #include "Engine.h"
 
 #include <iostream>
-#include <Windows.h>
-#include <conio.h>
+#include "Level/Level.h"
+
+/*
+*		특수 맴버 함수
+*/
+
+Engine::Engine()
+{
+}
+
+Engine::~Engine()
+{
+	if (_mainLevel != nullptr)
+	{
+		delete _mainLevel;
+		_mainLevel = nullptr;
+	}
+}
+
+/*
+*		공개 함수
+*/
 
 void Engine::Run()
 {
@@ -27,12 +47,35 @@ void Engine::Run()
 		ProcessInput();
 		if (deltaTime >= oneFrameTime)
 		{
-			Update(deltaTime);
+			BeginPlay();
+			Tick(deltaTime);
 			Render();
-
+			
+			// 시간 업데이트
 			previousTime = currentTime;
+
+			// 현재 프레임의 입력을 기록
+			for (int i = 0; i < 256; ++i)
+			{
+				_keyStates[i]._previousKeyDown = _keyStates[i]._isKeyDown;
+			}
 		}
 	}
+}
+
+bool Engine::GetKey(int keyCode)
+{
+	return _keyStates[keyCode]._isKeyDown;
+}
+
+bool Engine::GetKeyDown(int keyCode)
+{
+	return !_keyStates[keyCode]._previousKeyDown && _keyStates[keyCode]._isKeyDown;
+}
+
+bool Engine::GetKeyUp(int keyCode)
+{
+	return _keyStates[keyCode]._previousKeyDown && !_keyStates[keyCode]._isKeyDown;
 }
 
 void Engine::Quit()
@@ -41,19 +84,73 @@ void Engine::Quit()
 	isQuit = true;
 }
 
+void Engine::AddLevel(Level* newLevel)
+{
+	if (_mainLevel != nullptr)
+	{
+		delete _mainLevel;
+		_mainLevel = nullptr;
+	}
+	_mainLevel = newLevel;
+}
+
+/*
+*		Engine의 내부 함수
+*/
+
 void Engine::ProcessInput()
 {
+	for (int i = 0; i < 255; ++i)
+	{
+		_keyStates[i]._isKeyDown = GetAsyncKeyState(i) & 0x8000;
+	}
+
 	if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
 	{
 		Quit();
 	}
 }
 
-void Engine::Update(float deltaTime)
+void Engine::BeginPlay()
 {
-	std::cout << "DeltaTime: " << deltaTime << ", FPS: " << (1.0f / deltaTime) << "\n";
+	if (_mainLevel != nullptr)
+	{
+		_mainLevel->BeginPlay();
+	}
+}
+
+void Engine::Tick(float deltaTime)
+{
+	/*if (GetKeyDown('A'))
+	{
+		std::cout << "GetKeyDown('a')\n";
+	}
+	if (GetKey('A'))
+	{
+		std::cout << "GetKey('a')\n";
+	}
+
+	if (GetKeyUp('A'))
+	{
+		std::cout << "GetKeyUp('a')\n";
+	}*/
+	
+	if (_mainLevel != nullptr)
+	{
+		_mainLevel->Tick(deltaTime);
+	}
+
+	if (GetKeyDown(VK_ESCAPE))
+	{
+		Quit();
+	}
+	//std::cout << "DeltaTime: " << deltaTime << ", FPS: " << (1.0f / deltaTime) << "\n";
 }
 
 void Engine::Render()
 {
+	if (_mainLevel != nullptr)
+	{
+		_mainLevel->Render();
+	}
 }
--- Engine/Core/Engine.h
+++ Engine/Core/Engine.h
@@ -4,12 +4,13 @@
 #include "Core.h"
 #include <Windows.h>
 
+class Level;
 class ENGINE_API Engine
 {
 #pragma region 특수 맴버 함수
 public:
-	Engine()							= default;
-	~Engine()							= default;
+	Engine();
+	~Engine();
 
 public:
 	Engine(const Engine&)				= delete;
@@ -17,26 +18,50 @@ public:
 	Engine(Engine&&)					= delete;
 	Engine& operator=(Engine&&)			= delete;
 #pragma endregion
+	
+#pragma region KeyState_키_확인_구조체
+	struct KeyState
+	{
+		enum CONST_KEYSTATE
+		{
+			VK_MAX = 255
+		};
+		bool _isKeyDown = false;
+		bool _previousKeyDown = false;
+	};
+#pragma endregion
 
 #pragma region 공개 함수
 public:
 	// 엔진 실행 함수
 	void Run();
 
+	// 키 확인 함수
+	bool GetKey(int keyCode);
+	bool GetKeyDown(int keyCode);
+	bool GetKeyUp(int keyCode);
+
 	// 엔진 종료 함수
 	void Quit();
+
+	void AddLevel(Level* newLevel);
+
 #pragma endregion
 
 #pragma region 내부 함수
 private:
 	void ProcessInput();
-	void Update(float deltaTime = 0.0f);
+
+	void BeginPlay();
+	void Tick(float deltaTime = 0.0f);
 	void Render();
 #pragma endregion
 
 #pragma region 맴버 변수
 private:
 	bool isQuit = false;
+	KeyState _keyStates[256];
+	Level* _mainLevel = nullptr;
 #pragma endregion
 };
 
