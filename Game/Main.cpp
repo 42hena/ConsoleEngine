4af2a4d7aaf6eff12cf4e41c7cde9bf89e0b9ba6
soloplay <somnium1108@gmail.com>
2025-07-28 21:17:48 +0900

    Update

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "Engine.h"
 #include "Math/Vector2.h"
-#include "DemoLevel.h"
+#include "Demo/DemoLevel.h"
+#include "Level/SokobanLevel.h"
 #include <Windows.h>
 #define _CRTDBG_MAP_ALLOC
+using namespace std;
 
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	Engine engine;
-	engine.AddLevel(new DemoLevel());
+	engine.AddLevel(new SokobanLevel());
 	engine.Run();
 }
