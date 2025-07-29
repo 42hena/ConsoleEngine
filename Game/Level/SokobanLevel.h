9571d5061baeb6332379954ed47a35107e4b1f5d
soloplay <somnium1108@gmail.com>
2025-07-29 17:47:04 +0900

    Sokoban Level의 기능 추가
    - target과 currentScore를 비교하여 게임이 종료되었는지 확인하는 로직 구현
    - 플레이어가 실제로 움직일 수 있는지 판단하는 로직 구현
    - '1'도 벽으로 추가

--- Game/Level/SokobanLevel.h
+++ Game/Level/SokobanLevel.h
@@ -3,18 +3,40 @@
 
 #include "Level/Level.h"
 
-class SokobanLevel : public Level
+#include "Interface/ICanPlayerMove.h"
+
+class SokobanLevel : public Level, public ICanPlayerMove
 {
 	RTTI_DECLARATIONS(SokobanLevel, Level)
 
 #pragma region 특수 멤버 함수
 public:
 	SokobanLevel();
+	~SokobanLevel(){}
+#pragma endregion
+
+#pragma region 공개 함수
+private:
+	/* 이벤트 함수 */
+	virtual void Render() override;
+
+public:
+	/* 사용함수 */
+	bool CanPlayerMove(const Vector2& playerPosition, const Vector2& newPosition) override;
 #pragma endregion
 
 #pragma region 내부 함수	
 private:
+	/* 파일 맵 관련 함수 */
 	void ReadMapFile(const char* filename);
+
+	/* 게임 로직 관련 함수*/
+	bool CheckGameClear();
+#pragma endregion
+
+#pragma region 멤버 변수
+	int		_targetScore = 0;
+	bool	_isGameClear = false;
 #pragma endregion
 };
 
